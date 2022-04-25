7d86a344e7b112047de117b6b2163882d4ba74e5
Eva Lecocq <evalecocq@free.fr>
2022-04-25 16:24:05 +0200

    css

---
+++ public/controller/main.js
@@ -0,0 +1,10 @@
+//"use = strict";
+//
+//function main() {
+//  const modal = document.getElementById("modal");
+//  const modalOpen = document.getElementById("nav-link");
+//
+//  modalOpen.addEventListener("click", onDisplay);
+//}
+//
+//addEventListener("load", main);

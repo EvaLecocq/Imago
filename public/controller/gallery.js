ad3169025f7477ce357b8ffd3a72384614f39354
Eva Lecocq <evalecocq@free.fr>
2022-06-18 00:38:33 +0200

    css modif

--- public/controller/gallery.js
+++ public/controller/gallery.js
@@ -1,23 +1,23 @@
- const modal = document.querySelector(".modal");
- const previews = document.querySelectorAll(".gallery img");
- const img = document.querySelector(".full-img");
- const imgText = document.querySelector(".caption");
+const modal = document.querySelector(".modal");
+const previews = document.querySelectorAll(".gallery img");
+const img = document.querySelector(".full-img");
+const imgText = document.querySelector(".caption");
 
- previews.forEach((preview) => {
-    preview.addEventListener("click", () => {
-        modal.classList.add("open");
-        img.classList.add("open");
-        //Change text and img
-        const imgSrc = preview.getAttribute('src');
-        img.src = imgSrc;
-        const altText = preview.alt;
-        imgText.textContent = altText;
-    });
- });
+previews.forEach((preview) => {
+  preview.addEventListener("click", () => {
+    modal.classList.add("open");
+    img.classList.add("open");
+    //Change text and img
+    const imgSrc = preview.getAttribute("src");
+    img.src = imgSrc;
+    const altText = preview.alt;
+    imgText.textContent = altText;
+  });
+});
 
- modal.addEventListener('click', (e) => {
-    if(e.target.classList.contains("modal")) {
-        modal.classList.remove("open");
-        img.classList.add("open");
-    }
- })
+modal.addEventListener("click", (e) => {
+  if (e.target.classList.contains("modal")) {
+    modal.classList.remove("open");
+    img.classList.add("open");
+  }
+});

74a1f1efdb21be4518a5267476711c0edd7e2208
Eva Lecocq <evalecocq@free.fr>
2022-06-18 14:47:18 +0200

    reveal div

--- public/controller/main.js
+++ public/controller/main.js
@@ -1,13 +1,36 @@
 //Anim click
 
-window.addEventListener('click',(e) => {
-    const cercle = document.createElement('div');
-    cercle.className = 'clickAnim';
-    cercle.style.top = `${e.pageY - 20}px`;
-    cercle.style.left = `${e.pageX - 20}px`;
-    document.body.appendChild(cercle);
+window.addEventListener("click", (e) => {
+  const cercle = document.createElement("div");
+  cercle.className = "clickAnim";
+  cercle.style.top = `${e.pageY - 20}px`;
+  cercle.style.left = `${e.pageX - 20}px`;
+  document.body.appendChild(cercle);
 
-    setTimeout(() => {
-        cercle.remove();
-    }, 1500)
-})
+  setTimeout(() => {
+    cercle.remove();
+  }, 1500);
+});
+
+//Reveal div
+
+const ratio = 0.1;
+const options = {
+  root: null,
+  rootMargin: "0px",
+  threshold: ratio,
+};
+
+const handleIntersect = function (entries, observer) {
+  entries.forEach(function (entry) {
+    if (entry.intersectionRatio > ratio) {
+      entry.target.classList.add("reveal-visible");
+      observer.unobserve(entry.target);
+    }
+  });
+};
+
+const observer = new IntersectionObserver(handleIntersect, options);
+document.querySelectorAll(".reveal").forEach(function (r) {
+  observer.observe(r);
+});

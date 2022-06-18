6dcb8c4c9dd297ca70f21354aa17d99de3f20982
Eva Lecocq <evalecocq@free.fr>
2022-06-19 00:22:14 +0200

    Update snake.js

--- public/controller/snake.js
+++ public/controller/snake.js
@@ -1,6 +1,6 @@
 //Snake
 
-const board_border = "white";
+const board_border = "black";
 const board_background = "#222";
 const snake_col = 'white';
 
@@ -13,26 +13,46 @@ let snake = [
 ]
 
 let score = 0;
-// True if changing direction
-let changing_direction = false;
-// Horizontal velocity
-let food_x;
-let food_y;
-let dx = 10;
-// Vertical velocity
-let dy = 0;
+
+// snake direction // true = move direction
+//let changing_direction = false;
+
+// Pos x & y food
+//let food_x;
+//let food_y;
+
+// Pos x & y snake
+let pos_x = 10;
+let pos_y = 0;
 
 
-// Get the canvas element
+// canvas element in 2D
 const snakeCanv = document.querySelector(".snakeCanv");
-// Return a two dimensional drawing context
 const snakeCanv_ctx = snakeCanv.getContext("2d");
 // Start game
 main();
-
 gen_food();
 
-document.addEventListener("keydown", change_direction);
+// detect arrow keys
+document.addEventListener("keydown", (event)=> {
+
+    if (event.key === "ArrowLeft") {
+      pos_x = -10;
+      pos_y = 0;
+    }
+    if (event.key === "ArrowUp") {
+      pos_x = 0;
+      pos_y = -10;
+    }
+    if (event.key === "ArrowRight") {
+      pos_x = 10;
+      pos_y = 0;
+    }
+    if (event.key === "ArrowDown") {
+      pos_x = 0;
+      pos_y = 10;
+    }
+  });
 
 // main function called repeatedly to keep the game running
 function main() {
@@ -113,42 +133,11 @@ function gen_food() {
   });
 }
 
-function change_direction(event) {
-  const LEFT_KEY = 37;
-  const RIGHT_KEY = 39;
-  const UP_KEY = 38;
-  const DOWN_KEY = 40;
-  
-// Prevent the snake from reversing
 
-  if (changing_direction) return;
-  changing_direction = true;
-  const keyPressed = event.keyCode;
-  const goingUp = dy === -10;
-  const goingDown = dy === 10;
-  const goingRight = dx === 10;
-  const goingLeft = dx === -10;
-  if (keyPressed === LEFT_KEY && !goingRight) {
-    dx = -10;
-    dy = 0;
-  }
-  if (keyPressed === UP_KEY && !goingDown) {
-    dx = 0;
-    dy = -10;
-  }
-  if (keyPressed === RIGHT_KEY && !goingLeft) {
-    dx = 10;
-    dy = 0;
-  }
-  if (keyPressed === DOWN_KEY && !goingUp) {
-    dx = 0;
-    dy = 10;
-  }
-}
 
 function move_snake() {
   // Create the new Snake's head
-  const head = {x: snake[0].x + dx, y: snake[0].y + dy};
+  const head = {x: snake[0].x + pos_x, y: snake[0].y + pos_y};
   // Add the new head to the beginning of snake body
   snake.unshift(head);
   const has_eaten_food = snake[0].x === food_x && snake[0].y === food_y;

69b517f1a4538e4757993eeb86f973e356133860
Eva Lecocq <evalecocq@free.fr>
2022-06-18 22:30:01 +0200

    ControllerSnake + game Snake

---
+++ src/Controller/SnakeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SnakeController extends AbstractController
+{
+    #[Route('/snake', name: 'app_snake')]
+    public function index(): Response
+    {
+        return $this->render('snake/index.html.twig', [
+            'controller_name' => 'SnakeController',
+        ]);
+    }
+}

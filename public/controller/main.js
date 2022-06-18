8cb72fbd88e2a7c4e3a744f5364a5c67d14ea453
Eva Lecocq <evalecocq@free.fr>
2022-06-18 23:49:38 +0200

    Snake2

--- public/controller/main.js
+++ public/controller/main.js
@@ -21,8 +21,8 @@ const options = {
   threshold: ratio,
 };
 
-const handleIntersect = (entries, observer)=> {
-  entries.forEach((entry)=> {
+const handleIntersect = (entries, observer) => {
+  entries.forEach((entry) => {
     if (entry.intersectionRatio > ratio) {
       entry.target.classList.add("reveal-visible");
       observer.unobserve(entry.target);
@@ -31,82 +31,6 @@ const handleIntersect = (entries, observer)=> {
 };
 
 const observer = new IntersectionObserver(handleIntersect, options);
-document.querySelectorAll(".reveal").forEach((r)=> {
+document.querySelectorAll(".reveal").forEach((r) => {
   observer.observe(r);
 });
-
-//Snake
-
-window.onload = ()=> {
-  canv = document.querySelector(".snakeCanv");
-  ctx = canv.getContext("2d");
-  document.addEventListener("keydown", keyPush);
-  setInterval(game, 1000 / 15);
-};
-
-px = py = 10;
-gs = tc = 20;
-ax = ay = 15;
-xv = yv = 0;
-trail = [];
-tail = 5;
-
-//Keyboard function
-function game() {
-  px += xv;
-  py += yv;
-  if (px < 0) {
-    px = tc - 1;
-  }
-  if (px > tc - 1) {
-    px = 0;
-  }
-  if (py < 0) {
-    py = tc - 1;
-  }
-  if (py > tc - 1) {
-    py = 0;
-  }
-  ctx.fillStyle = "#222";
-  ctx.fillRect(0, 0, canv.width, canv.height);
-  ctx.fillStyle = "lime";
-  for (var i = 0; i < trail.length; i++) {
-    ctx.fillRect(trail[i].x * gs, trail[i].y * gs, gs - 2, gs - 2);
-    if (trail[i].x == px && trail[i].y == py) {
-      tail = 5;
-    }
-  }
-  trail.push({ x: px, y: py });
-  while (trail.length > tail) {
-    trail.shift();
-  }
-
-  if (ax == px && ay == py) {
-    tail++;
-    ax = Math.floor(Math.random() * tc);
-    ay = Math.floor(Math.random() * tc);
-  }
-  ctx.fillStyle = "red";
-  ctx.fillRect(ax * gs, ay * gs, gs - 2, gs - 2);
-}
-
-function keyPush(e) {
-  switch (e.keyCode) {
-    case 37:
-      xv = -1;
-      yv = 0;
-      break;
-    case 38:
-      xv = 0;
-      yv = -1;
-      break;
-    case 39:
-      xv = 1;
-      yv = 0;
-      break;
-    case 40:
-      xv = 0;
-      yv = 1;
-      break;
-  }
-}
